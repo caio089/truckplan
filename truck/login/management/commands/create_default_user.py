31582d69c2a3c107a81c7963f514e18df607e415
caiocampos <ccamposs2007@gmail.com>
2025-09-24 12:05:04 -0300

    1234

--- truck/login/management/commands/create_default_user.py
+++ truck/login/management/commands/create_default_user.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -10,11 +11,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            default_password = os.environ.get('DEFAULT_ADMIN_PASSWORD', 'lucas10')
+
             if not User.objects.filter(username='admin').exists():
                 user = User.objects.create_user(
                     username='admin',
                     email='admin@truckplan.com',
-                    password='TruckPlan2024!',
+                    password=default_password,
                     is_staff=True,
                     is_superuser=True,
                     first_name='Administrador',
@@ -24,13 +27,22 @@ class Command(BaseCommand):
                     self.style.SUCCESS(
                         f'Usuário padrão criado com sucesso!\n'
                         f'Usuário: admin\n'
-                        f'Senha: TruckPlan2024!'
+                        f'Senha: {default_password}'
                     )
                 )
                 logger.info('Usuário padrão criado: admin')
             else:
+                user = User.objects.get(username='admin')
+                user.set_password(default_password)
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
                 self.stdout.write(
-                    self.style.WARNING('Usuário padrão já existe!')
+                    self.style.SUCCESS(
+                        f'Senha do usuário admin redefinida com sucesso!\n'
+                        f'Usuário: admin\n'
+                        f'Nova senha: {default_password}'
+                    )
                 )
         except IntegrityError as e:
             self.stdout.write(
